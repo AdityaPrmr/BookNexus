a37e404ecb68dd891028f60466bd34bf63d4eeb6
AdityaPrmr <parmar2100parmar@gamil.com>
2025-03-27 22:00:20 +0530

    msg

--- BookStore/wwwroot/js/major.js
+++ BookStore/wwwroot/js/major.js
@@ -64,7 +64,7 @@ function fetchAndDisplayPdf(path) {
         }
     })
         .then(response => {
-            if (!response.ok) throw new Error('Network response was not ok');
+            if (!response.ok) throw new Error(response.message);
             return response.blob();
         })
         .then(blob => {
